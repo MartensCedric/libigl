fd1cfa14aa0ef3cb22ff06ac61d53125d3dfdb23
Cedric Martens <cedricmartens98@gmail.com>
2025-07-14 21:02:45 -0400

    Fix test

--- tests/include/igl/one_shot_winding_number.cpp
+++ tests/include/igl/one_shot_winding_number.cpp
@@ -1,12 +1,12 @@
 #include <test_common.h>
 #include <igl/one_shot_winding_number.h>
-
+#include <iostream>
 
 TEST_CASE("one_shot_winding_number: Weighted Sum", "[igl]")
 {
-  Eigen::MatrixX2d A;
-  Eigen::VectorXd leftA;
-  Eigen::VectorXd rightA;
+  Eigen::MatrixX2d A(5,2);
+  Eigen::VectorXd leftA(5);
+  Eigen::VectorXd rightA(5);
 
   leftA << 0.3, 0.2, 0.1, 0.9, 0.5;
   rightA << 0.7, 0.8, 0.9, 0.1, 0.5;
@@ -14,9 +14,9 @@ TEST_CASE("one_shot_winding_number: Weighted Sum", "[igl]")
   A.col(0) = leftA;
   A.col(1) = rightA;
 
-  Eigen::MatrixX2i X;
-  Eigen::VectorXi leftX;
-  Eigen::VectorXi rightX;
+  Eigen::MatrixX2i X(5,2);
+  Eigen::VectorXi leftX(5);
+  Eigen::VectorXi rightX(5);
 
   leftX << 0, 0, 1, 2, -1;
   rightX << 1, -1, 0, 1, 0;
@@ -24,10 +24,10 @@ TEST_CASE("one_shot_winding_number: Weighted Sum", "[igl]")
   X.col(0) = leftX;
   X.col(1) = rightX;
 
-  Eigen::VectorXd gt;
+  Eigen::VectorXd gt(5);
   gt << 0.7, -0.8, 0.1, 1.9, -0.5;
 
-  Eigen::VectorXd W;
+  Eigen::VectorXd W(5);
   igl::one_shot_winding_number(A, X, W);
   test_common::assert_near(W, gt, 1e-12);
 
